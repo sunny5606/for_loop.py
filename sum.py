458aca656014fb0e860927a3a8e3211e7c72388d
sunny5606 <24jr1a05b56@gmail.com>
2026-04-17 22:28:12 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,18 @@
+a=3
+b=2
+c=1
+d=a+b
+print((a+b+c)/3) #average
+print(d)
+
+# list
+l=["apple","banana","dates"]
+print(l[::-1])
+m_l=["blue","indigo","black","green"]
+l.extend(m_l) # or concatenate k=l+m_l
+print(l)
+k=l+m_l
+print(k)
+
+t1=(1,2,3)
+t2=(4,5,6)
