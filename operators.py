4f8c3deff6286eceb6297156abf8e79727a4d538
sunny5606 <24jr1a05b56@gmail.com>
2026-04-17 22:28:59 +0530

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,48 @@
+# Arithmetic Operators
+a = 10
+b = 3
+
+print("Arithmetic Operators:")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("a//b:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+#comparison Operators
+print("\nComparison Operators:")
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
+
+# Logical Operators
+print("\nLogical Operators:")
+x = True
+y = False
+print("x and y:", x and y)
+print("x or y:", x or y)
+print("not x:", not x)
+
+# Assignment Operators
+print("\nAssignment Operators:")
+c = 5
+c += 2
+print("c after += 2:", c)
+c *= 3
+print("c after *= 3:", c)
+
+# Bitwise Operators
+print("\nBitwise Operators:")
+m = 5  # 0101
+n = 3  # 0011
+print("m & n:", m & n)
+print("m | n:", m | n)
+print("m ^ n:", m ^ n)
+print("~m:", ~m)
+print("m << 1:", m << 1)
+print("m >> 1:", m >> 1)
