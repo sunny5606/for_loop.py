0333fdc3970fa47b343c4f6c7098b44880cf6269
sunny5606 <24jr1a05b56@gmail.com>
2026-04-17 22:15:43 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,18 @@
+# In for loop we use range function range(start,stop,step_size)
+for i in range(1,11):
+    print(i)
+
+# list using for() loop
+l=[1,2,"shamshuddin","Hello"]
+for i in l:
+    print(i)
+
+# tuple using for() loop
+t=("APPLE","BANANA","ORANGE",)
+for i in t:
+    print(i)
+
+# string using for() loop
+s='shamshuddin'
+for i in s:
+    print(i)
