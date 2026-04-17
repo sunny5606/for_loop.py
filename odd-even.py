aaae8fc67dc973885f51ce4469303d63d45247f4
sunny5606 <24jr1a05b56@gmail.com>
2026-04-17 22:22:30 +0530

    Create odd-even.py

---
+++ odd-even.py
@@ -0,0 +1,15 @@
+# checking even or odd
+n=int(input("Enter value: "))
+
+if(n % 2 == 0):
+    print(f"{n} is even")
+else:
+    print(f"{n} is odd")
+
+# checking multiples of 7
+x = int(input("Enter: "))
+
+if(x % 7 == 0):
+    print("ok.multiple")
+else:
+    print("Not ok.")    
